0d0b89f2ae88c2d0d6358df1eca9d4fb73cbd9fb
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-06-26 23:26:01 +0530

    Created express server and small crud application

---
+++ 02_ExpressJs/index.js
@@ -0,0 +1,77 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+
+// Some basic route examples
+app.get('/', (req, res) => {
+    res.send("Go to register route!");
+})
+
+app.get("/login", (req, res) => {
+    res.send("Send some data at credentials route");
+})
+
+app.get("/register", (req, res) => {
+    res.send("To login go to login route");
+})
+
+
+// To access data from URL
+app.use(express.json());
+
+// To store data into array
+let Data = [];
+let nextId = 1;
+
+// To send data to route
+app.post("/credentials", (req, res) => {
+    const {name, age} = req.body;
+    const newData = {id:nextId++, name, age};
+    Data.push(newData);
+    res.status(201).send(newData);
+});
+
+// To retrieve object from route
+app.get("/credentials", (req, res) => {
+    res.status(200).send(Data);
+})
+
+// To retrieve specific object from route
+app.get("/credentials/:id", (req, res) => {
+    const object = Data.find(obj => obj.id === parseInt(req.params.id));
+    (!object) ? res.status(404).send("Could not find record!") : res.status(200).send(object);
+})
+
+// To update specific object
+app.put("/credentials/:id", (req, res) => {
+    const object = Data.find(obj => obj.id === parseInt(req.params.id));
+    
+    if (!object) {
+        return res.status(404).send("Could not find record!");
+    }
+
+    const { name, age } = req.body;
+    object.name = name;
+    object.age = age;
+    res.status(200).send(object);
+})
+
+// To delete specific object
+app.delete("/credentials/:id", (req, res) => {
+    const index = Data.findIndex(obj => obj.id === parseInt(req.params.id));
+    
+    if (index === -1) {
+        return res.status(404).send("Could not find record!");
+    }
+
+    Data.splice(index, 1);
+    res.status(200).send("Record has deleted!");
+})
+
+// To listen server on specified port
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
