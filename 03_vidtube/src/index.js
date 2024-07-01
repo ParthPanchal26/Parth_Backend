a46649c67fa8ebbdb3547770c8c04081eb21882c
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-07-01 12:48:09 +0530

    Created vidtube database in mongoDB and established successful connection with the DB.

--- 03_vidtube/src/index.js
+++ 03_vidtube/src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello, World! This is from src/index.js");
+// import app from "./app.js";
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import { connectDB } from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+})
+
+const PORT = process.env.PORT || 3001;
+
+connectDB()
+.then((result) => {
+    app.listen(PORT, (req, res) => {
+        console.log(`Server listening on port ${PORT}`);
+    })
+}).catch((err) => {
+    console.log(`Database connection error ! ${err}`);
+});
